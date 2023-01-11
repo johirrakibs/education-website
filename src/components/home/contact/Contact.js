54f09ad72c62268a7038a396106ded357a35364b
johirrakibs <johir.rakib59@gmail.com>
2023-01-12 00:06:39 +0600

    dfghjk

--- src/components/home/contact/Contact.js
+++ src/components/home/contact/Contact.js
@@ -27,7 +27,7 @@ const Contact = () => {
               </div>
               <div className='box'>
                 <h4>PHONE:</h4>
-                <p> + 1235 2355 98</p>
+                <p> 01637543661</p>
               </div>
             </div>
 
