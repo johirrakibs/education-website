9afe77a9f1dd985d851c51ff843629c2f0516b75
johirrakibs <johir.rakib59@gmail.com>
2023-01-12 01:06:23 +0600

    change mail

--- src/components/home/contact/Contact.js
+++ src/components/home/contact/Contact.js
@@ -23,7 +23,7 @@ const Contact = () => {
               </div>
               <div className='box'>
                 <h4>EMAIL:</h4>
-                <p> info@yoursite.com</p>
+                <p> johir.rakib59@gmail.com</p>
               </div>
               <div className='box'>
                 <h4>PHONE:</h4>
